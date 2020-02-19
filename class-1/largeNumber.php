c3f002bb4b18bb0d87f5e4cfcbcfc8d50d934e98
shanto rahman <shantoxdp@gmail.com>
2020-02-19 22:56:56 +0600

    logic change

--- class-1/largeNumber.php
+++ class-1/largeNumber.php
@@ -4,21 +4,17 @@
 
 function largeNumber($num1,$num2,$num3){
 
-    if($num1>$num2){
-        if($num1>$num3){
-            echo $num1;
-        }
-    }elseif($num2>$num1){
-        if($num2>$num3){
-            echo $num2;
-        }else{
-            echo $num3;
-        }
+    if($num1>$num2 && $num1>$num3){
+        echo $num1;
+    }elseif($num2>$num1 && $num2>$num3){
+        echo $num2;
+    }elseif($num3>$num1 && $num3>$num2){
+        echo $num3;
     }
 
 }
 
-largeNumber(1000,225,885);
+largeNumber(1000,225,8885);
 
 
 
--- class-1/smallNumber.php
+++ class-1/smallNumber.php
@@ -4,29 +4,15 @@
 
 function smallNumber($num1, $num2,$num3){
 
-    // if($num1<$num2){
-    //     if($num1<$num3){
-    //         echo $num1;
-    //     }
-    // }elseif($num2<$num1){
-
-    //     if($num2<$num3){
-    //         echo $num2;
-            
-    //     }else{
-    //         echo $num3;
-    //     }
-
-    // }
 
 
-    // // if($num1<$num2 || $num1<$num3){
-    // //     echo $num1;
-    // // }elseif($num2<$num1 || $num2<$num3){
-    // //     echo $num2;
-    // // }elseif($num3<$num1 || $num3<$num2){
-    // //     echo $num3;
-    // // }
+    if($num1<$num2 && $num1<$num3){
+        echo $num1;
+    }elseif($num2<$num1 && $num2<$num3){
+        echo $num2;
+    }elseif($num3<$num1 && $num3<$num2){
+        echo $num3;
+    }
 
 }
 
