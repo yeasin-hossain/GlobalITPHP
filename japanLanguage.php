256ac9f76beb6b78d3c4ce659b73b6cd7626c251
shanto rahman <shantoxdp@gmail.com>
2020-02-23 23:57:32 +0600

    add japanese latter converter

---
+++ japanLanguage.php
@@ -0,0 +1,58 @@
+<?php 
+
+   $data='{ 
+       "author":"Shanto",
+       "a":"in japanese latter:- あ ",
+       "i":"in japanese latter:- い",
+       "u":"in japanese latter:- う",
+       "e":"in japanese latter:- え",
+       "o":"in japanese latter:- お",
+       "ka":"in japanese latter:- か",
+       "ki":"in japanese latter:- き",
+       "ku":"in japanese latter:- く",
+       "ke":"in japanese latter:- け",
+       "ko":"in japanese latter:- こ",
+       "sa":"in japanese latter:- さ",
+       "shi":"in japanese latter:- し",
+       "su":"in japanese latter:- す",
+       "se":"in japanese latter:- せ",
+       "so":"in japanese latter:- そ",
+       "ta":"in japanese latter:- た",
+       "chi":"in japanese latter:- ち",
+       "tsu":"in japanese latter:- つ",
+       "te":"in japanese latter:- て",
+       "to":"in japanese latter:- と",
+       "na":"in japanese latter:- な",
+       "ni":"in japanese latter:- に",
+       "nu":"in japanese latter:- ぬ",
+       "ne":"in japanese latter:- ね",
+       "no":"in japanese latter:- の",
+       "ha":"in japanese latter:- は",
+       "hi":"in japanese latter:- ひ",
+       "fu":"in japanese latter:- ふ",
+       "he":"in japanese latter:- へ",
+       "ho":"in japanese latter:- ほ",
+       "ma":"in japanese latter:- ま",
+       "mi":"in japanese latter:- み",
+       "mu":"in japanese latter:- む",
+       "me":"in japanese latter:- め",
+       "mo":"in japanese latter:- も",
+       "ya":"in japanese latter:- や",
+       "yu":"in japanese latter:- ゆ",
+       "yo":"in japanese latter:- よ",
+       "ra":"in japanese latter:- ら",
+       "ri":"in japanese latter:- り",
+       "ru":"in japanese latter:- る",
+       "re":"in japanese latter:- れ",
+       "ro":"in japanese latter:- ろ",
+       "wa":"in japanese latter:- わ",
+       "wi":"in japanese latter:- ( ゐ )",
+       "we":"in japanese latter:- ( ゑ )",
+       "wo":"in japanese latter:-を"
+    }';
+
+        $all=json_decode($data);
+        echo $all-> ro;
+        
+
+?>
