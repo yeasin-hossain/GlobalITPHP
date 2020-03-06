28c9690a7716b5c449d8c418bc585bc708884e85
shanto rahman <shantoxdp@gmail.com>
2020-03-06 23:35:17 +0600

    problem was in count function, now replace with emty fn

--- class-7/calculate.php
+++ class-7/calculate.php
@@ -39,7 +39,12 @@ if(isset($_GET['edit'])){
     $update = true;
     $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error);
 
-    if(count($result )==  1){
+    // if(count($result)==  1){
+    //     $row = $result->fetch_array();
+    //     $name = $row['name'];
+    //     $location = $row ['location'];
+    // }
+    if(!empty($result)){
         $row = $result->fetch_array();
         $name = $row['name'];
         $location = $row ['location'];
